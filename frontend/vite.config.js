63572ce45530331f7ffb14a422c36f095d9bf1df
Yohanna S. Perez <yohperez@gmail.com>
2026-09-09 13:04:39 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+ 
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: 4173,
+    port: 5173,
+  },
+  preview: {
+    host: true,
+    allowedHosts: ["frontend-civicmind.up.railway.app"],
   },
 });
